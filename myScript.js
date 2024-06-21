521f060debfeb57adbe97cf764b3f361fcd7690f
Eliana5967 <elianankebede@gmail.com>
2024-06-21 17:08:33 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,7 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="This is fun and easy ";
 }
+function changeImage() {
+    var image = document.getElementById('myImage');
+    image.src = 'https://i.pinimg.com/originals/69/2c/42/692c4225798ffb3b4febe4e0899d282a.jpg';
+}
